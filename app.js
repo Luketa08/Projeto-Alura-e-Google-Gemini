4f6aa5196141539db034df4dd1bcfe8b3a32999e
Luketa08 <ferrazlucca@yahoo.com.br>
2024-09-07 15:11:22 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+function pesquisar() {
+    // Obtém a referência para a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    console.log(section); // **Para fins de depuração, mostra a seção no console**
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    
+  // se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    // então não mostre nada
+    section.innerHTML = "<p>Busca vazia<p>"
+    return
+  }
+  // deixando as palavras do campoPesquisa em minusculo 
+  campoPesquisa = campoPesquisa.toLowerCase()
+  
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let aconteceu = "";
+    let mudou = "";
+    let tags = ""; 
+     // Para cada dado na lista de dados, construir o HTML do resultado
+    for (let dado of dados) {
+      // deixando tudo em minuscuol
+      titulo = dado.titulo.toLocaleLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      aconteceu = dado.aconteceu.toLowerCase()
+      mudou = dado.mudou.toLowerCase()
+      tags = dado.tags.toLowerCase()
+    
+      // Se titulo ou a descrição ou oque aconteceu ou oq mudou ou as tags tem o campoPesquisa
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)  || aconteceu.includes(campoPesquisa) || mudou.includes(campoPesquisa)|| tags.includes(campoPesquisa)) 
+        {
+      // Cria um novo elemento HTML para cada resultado
+      resultados += `
+      <div class="item-resultado">
+        <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <h3> O que aconteceu</h3>
+        <p class="aconteceu-meta">${dado.aconteceu}</p>
+        <h3> O que mudou na aviação</h3>
+        <p class="mudou-meta">${dado.mudou} </br>
+        <a href="${dado.link}" target="_blank">Veja mais sobre</a>
+      </div>`;
+       } 
+
+  // Atribui o HTML gerado para a seção de resultados
+  section.innerHTML = resultados;
+      }
+   }
---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados = [
+    {  
+        titulo: "A Tragédia Aérea de Nova York: A Colisão entre os Voos TWA 266 e United 826",
+        descricao: "Em 16 de dezembro de 1960, um dos piores acidentes aéreos da história ocorreu nos céus de Nova York. Dois grandes aviões de passageiros, um Douglas DC-8 da United Airlines (voo 826) e um Lockheed L-1049 Super Constellation da Trans World Airlines (voo 266), colidiram em pleno ar, causando a morte de todas as 128 pessoas a bordo e de seis pessoas em terra.",
+        aconteceu: "A colisão ocorreu durante uma névoa densa que cobria a cidade de Nova York. Os dois aviões estavam se aproximando do Aeroporto LaGuardia, e devido às condições climáticas e a um erro de comunicação, as aeronaves se chocaram em pleno ar. Um dos aviões caiu em Staten Island, enquanto o outro caiu em Park Slope, no Brooklyn.",
+        mudou: "Melhoria dos sistemas de radar: Os sistemas de radar foram aprimorados para oferecer maior precisão e confiabilidade. </br> Padronização dos procedimentos: Os procedimentos de comunicação e controle de tráfego aéreo foram padronizados e aperfeiçoados </br> Aumento da segurança: As investigações sobre o acidente levaram a uma série de medidas para aumentar a segurança da aviação civil.",
+        link: "https://www.youtube.com/watch?v=02keq6ERRZQ",
+        tags: "Colisão aérea Nova York 1960, colisao area nova york, 1960, 16 de dezembro de 1960, Douglas DC-8 Lockheed L-1049, Erro humano na aviação, TWA 266 United 826, EUA"
+      },
+      {  
+        titulo: "O Voo 191 da Delta Airlines: Uma Tragédia Causada por Forças da Natureza",
+        descricao: "O Voo 191 da Delta Airlines foi uma rota doméstica nos Estados Unidos que ligava Fort Lauderdale a Los Angeles, com escala em Dallas/Fort Worth. No dia 2 de agosto de 1985, este voo se tornou um dos piores acidentes aéreos da história dos Estados Unidos.",
+        aconteceu: "Ao se aproximar do aeroporto de Dallas, o avião, um Lockheed L-1011 TriStar, entrou em uma zona de turbulência extrema causada por um microburst. Um microburst é uma violenta rajada descendente de ar, que pode causar mudanças repentinas e drásticas na velocidade e direção do vento. </br> Os pilotos, despreparados para lidar com a força do microburst, perderam o controle da aeronave. O avião caiu verticalmente, chocando-se contra o solo a cerca de um quilômetro da pista. A aeronave atingiu um carro e dois tanques de água, causando uma explosão, morrendo 137 pessoas",
+        mudou: "Treinamento de pilotos: Os pilotos passaram a receber treinamento mais completo sobre condições meteorológicas adversas, como microbursts. </br> Equipamentos de bordo: Os aviões foram equipados com instrumentos mais precisos para detectar e evitar microbursts. </br> Procedimentos de aproximação: Os procedimentos de aproximação para pouso foram revisados, com o objetivo de aumentar a segurança em condições climáticas adversas. ",
+        link: "https://www.youtube.com/watch?v=tu4mE93Iq5c",
+        tags: "Voo 191 Delta Airlines, Microburst, Meteorologia, 2 de agosto de 1985, EUA"
+      },
+      {  
+        titulo: "O Voo TAM 3054: Uma Tragédia que Marcou a Aviação Brasileira",
+        descricao: "O Voo TAM 3054 foi uma rota doméstica brasileira, operada pela TAM Linhas Aéreas (atual LATAM Airlines Brasil), que ligava Porto Alegre a São Paulo. No dia 17 de julho de 2007, este voo sofreu um dos maiores acidentes aéreos da história do Brasil.",
+        aconteceu: "Um Airbus A320-233, que operava o voo 3054, não conseguiu parar na pista do Aeroporto de Congonhas, em São Paulo, pois estava molhada devido à chuva, o que prejudicou a frenagem da aeronave. O avião ultrapassou a cabeceira da pista, atravessou a Avenida Washington Luís e colidiu com um prédio da própria TAM. O acidente resultou em 199 mortes, entre passageiros, tripulantes e pessoas que estavam no prédio",
+        mudou: "Revisão das normas de segurança: A Agência Nacional de Aviação Civil (ANAC) revisou e aperfeiçoou as normas de segurança para operação de aeronaves, especialmente em relação à frenagem em pistas molhadas. </br> Melhoria da infraestrutura aeroportuária: Os aeroportos brasileiros passaram por diversas melhorias, como a ampliação de pistas, a instalação de novos equipamentos de segurança e a modernização dos sistemas de controle de tráfego aéreo. </br> Treinamento de tripulações: Os programas de treinamento de pilotos e comissários de bordo foram aprimorados, com maior ênfase em situações de emergência e na prevenção de acidentes. </br> Conscientização sobre a segurança: O acidente serviu como um alerta para a importância da segurança na aviação e mobilizou a sociedade a exigir melhorias nas condições de operação dos aeroportos e das companhias aéreas.",
+        link: "https://www.youtube.com/watch?v=L1CvMA5j-y8",
+        tags: "TAM 3054, tam, 3054, Acidente aéreo Brasil, brasil, Brasil, 17 de julho de 2007, A320, chuva, sao paulo, são paulo, congonhas, sp, SP"
+      },
+      {  
+        titulo: "A Colisão entre o Voo Gol 1907 e um Legacy",
+        descricao: "O Voo Gol 1907 foi um dos maiores acidentes aéreos da história do Brasil. Ocorreu no dia 29 de setembro de 2006, quando um Boeing 737-800 da Gol Linhas Aéreas, que fazia a rota Manaus-Rio de Janeiro, colidiu em pleno ar com um jato executivo Legacy 600.",
+        aconteceu: "A colisão em pleno ar entre um Boeing 737-800 da Gol Linhas Aéreas e um jato executivo Legacy 600 resultou na perda de 154 vidas. Houve falha na comunicação e identificação de objetos na rota de colisão.",
+        mudou: "Melhoria dos sistemas de vigilância: Foram implementados novos radares e sistemas de vigilância, que permitem um acompanhamento mais preciso das aeronaves em voo. </br> Obrigatoriedade do transponder: O uso do transponder se tornou obrigatório para todas as aeronaves que operam no espaço aéreo brasileiro. </br> Revisão dos procedimentos de controle de tráfego aéreo: Os procedimentos de controle de tráfego aéreo foram revisados e aperfeiçoados, com o objetivo de evitar novas colisões. </br> Aumento da segurança: As empresas aéreas brasileiras investiram em medidas de segurança para evitar novos acidentes. </br> Cooperação internacional: O acidente levou a uma maior cooperação entre os órgãos de aviação civil de diferentes países, com o objetivo de estabelecer padrões de segurança mais elevados. ",
+        link: "https://www.youtube.com/watch?v=3jT9bjVf3Bo&themeRefresh=1",
+        tags: "Voo Gol 1907, Colisão, colisao, Boeing 737, boeing, legavy, legacy 600, 737, brasil, 29 de setembro de 2006, cominicação, falha, choque, brasil, Brasil, Rio de Janeiro, RJ, rj, rio de janeiro"
+      },
+]
